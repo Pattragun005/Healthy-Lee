1d146c0dc15ac59cae90e33364b12362447ea8ac
Pattragun005 <s5830213005@phuket.psu.ac.th>
2018-08-23 10:56:43 +0700

    Import project.
    Demo version 1.0

---
+++ Healthy Lee 2D/Assets/GameController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	[Header("Background")]
+	public List<Background> background;
+	private Transform bgTarget;
+	private int countBG = 0;
+
+	void Awake()
+	{
+		bgTarget = GameObject.Find("Background").transform;
+	}
+
+	void Start()
+	{
+		LoadBuilding(0,0);
+	}
+
+	void Update()
+	{
+		if(bgTarget.childCount < 3)
+		{
+			
+			if (++countBG >= background.Count) 
+				countBG = 0;
+			
+			float next = bgTarget.GetChild (bgTarget.childCount - 1).transform.position.x;
+			LoadBuilding (countBG,next + 11.7f);
+
+		}
+	}
+
+
+	public void LoadBuilding(int index, float next)
+	{
+		Vector3 pos = bgTarget.position;
+		pos.x += next;
+		pos.y += background [index].posY;
+		Instantiate (background [index].BG, pos, Quaternion.identity, bgTarget.transform);
+	}
+
+	[System.Serializable]
+	public class Background
+	{
+		public GameObject BG;
+		public float posY;
+	}
+		
+}		
+
---
+++ Healthy Lee 2D/Assets/Parallax.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour {
+
+	public float speed = 1f;
+
+	void FixedUpdate () {
+		transform.Translate(Vector3.left * Time.deltaTime * speed); 
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if(other.CompareTag("Destroy")){
+			Destroy (this.gameObject);
+		}
+	}
+
+}
